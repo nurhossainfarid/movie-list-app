82e8338cc063e342c7182496bab901004194d222
nurhossainfarid <faahsan1516@gmail.com>
2025-02-12 17:46:56 +0600

    attacted file

--- src/app/movie/movie-details/movie-details.component.ts
+++ src/app/movie/movie-details/movie-details.component.ts
@@ -15,6 +15,9 @@ export class MovieDetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   movieService = inject(MovieService);
   movieData: MovieType | undefined;
+  file: File | null = null;
+  fileUrl: string | null = null;
+  fileContent: string = '';
 
   constructor() {
     const movieId = this.route.snapshot.params['id'];
@@ -22,11 +25,32 @@ export class MovieDetailsComponent {
       .getMovieById(movieId)
       .then((movieData: MovieType | undefined) => {
         if (movieData) {
-          console.log(movieData);
           this.movieData = movieData;
         } else {
           console.error('Movie data not found');
         }
       });
+  }
+
+  // Handle file selection
+  onFileSelected(event: any) {
+    this.file = event.target.files[0];
+
+    if (this.file) {
+      const reader = new FileReader();
+
+      // Show image, video
+      if (this.file.type.includes('image')) {
+        reader.onload = () => {
+          this.fileUrl = reader.result as string;
+        };
+        reader.readAsDataURL(this.file);
+      } else if (this.file.type.includes('video')) {
+        reader.onload = () => {
+          this.fileUrl = reader.result as string;
+        };
+        reader.readAsDataURL(this.file);
+      }
+    }
   }
 }
